6617e96d1213454c60d84bb330663ac9fd64c883
Keith Marble <kmarble96@gmail.com>
2020-09-05 13:03:43 -0400

    Start with universally usable GraphQL boilerplate with schema stitching and domain separation

---
+++ src/models/index.js
@@ -0,0 +1,30 @@
+let users = {
+  1: {
+    id: "1",
+    username: "Keith",
+    messageIds: [1]
+  },
+  2: {
+    id: "2",
+    username: "Marble",
+    messageIds: [2]
+  }
+};
+
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World",
+    userId: "1"
+  },
+  2: {
+    id: "2",
+    text: "Bye world",
+    userId: "2"
+  }
+};
+
+export default {
+  users,
+  messages
+};
